0ce3d5fc4fb28d4d198bf18317cc4845ea1dba20
Tseng <mag940316@gmail.com>
2025-09-16 11:45:08 +0800

    1145

--- test.cpp
+++ test.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 int  main(){
-    cout << "hello world" << endl;
+    cout << "===================================" << endl;
+    cout << "name : Tai-Hao-Tseng" << endl;
+    cout << "grade : 1" << endl;
+    cout << "email : e24141068@gs.ncku.edu.tw" << endl;
+    cout << "favorite course : intro to cs" << endl;
+    cout << "===================================" << endl;
     return 0;
 }
